17fbdc4c3aeb5b2236ef3745dc1ccf324e5a19a3
Victoria <Victoria.Afe1@gmail.com>
2025-09-02 22:08:13 -0400

    Made webpages

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("click", function () {
+    const audio = document.getElementById("debsound");
+    audio.play().catch((err) => {
+        console.log("Audio play failed:", err);
+    });
+}, { once: true }); 
+
+<debatecom.html onclick ="window.location.href='debatecom.html'">  debatecom  </debatecom.html>
